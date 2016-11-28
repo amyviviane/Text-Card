67a747a920c0a6f5f0f1a5d681f81e8506c4ba14
ShihYao <judy9270564@gmail.com>
2016-11-29 01:58:07 +0800

    新增開始畫面及去除按鈕灰灰部分

--- app/src/main/java/com/example/amylilian/text_card/SplashScreen.java
+++ app/src/main/java/com/example/amylilian/text_card/SplashScreen.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public class SplashScreen extends AppCompatActivity {
@@ -13,6 +15,9 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        //        WindowManager.LayoutParams.FLAG_FULLSCREEN);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {

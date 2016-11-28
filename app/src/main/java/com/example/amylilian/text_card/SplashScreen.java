8d77249d8211390e4de6db17cea6c1cea1f81444
ShihYao <judy9270564@gmail.com>
2016-11-28 12:46:17 +0800

    暫存

---
+++ app/src/main/java/com/example/amylilian/text_card/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.example.amylilian.text_card;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent().setClass(SplashScreen.this, MainActivity.class));
+            }
+        }, 3000);
+
+    }
+}
